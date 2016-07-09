8d2688bfe9bc13f583c1fe5cbf974e9822b28880
BenjaminGutierrez <j_g572@txstate.edu>
2016-07-09 15:55:26 -0500

    redesign project

--- videoInventory/src/videoInventory/main.java
+++ videoInventory/src/videoInventory/main.java
@@ -1,13 +1,11 @@
-
-package storeInv;
-import java.io.*;
+package videoInventory;
 import java.util.Scanner;
 
-public class Main {
+public class main {
 	
 	private static Scanner scan;
-
-	public static void displayMenu(){
+	public static void displayMenu()
+	{
 		System.out.println("Video Store Inventory Menu");
 		System.out.println("1. Add Movie");
 		System.out.println("2. Remove Movie");
@@ -16,15 +14,15 @@ public class Main {
 		System.out.println("5. Quit the Program");
 		System.out.println("Enter Your Choice:");
 	}
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{		
 		int choice = 0;
 		int SKU, quantity;
 		float price;
 		String title;
 		scan = new Scanner (System.in);
+		Inventory inv = new Inventory();
 		
-	
 		while(choice != 5)
 		{
 			displayMenu();
@@ -32,63 +30,56 @@ public class Main {
 			
 			if(choice == 1)
 			{
-	
 				System.out.println("Enter the SKU: ");
 				SKU = scan.nextInt();
-			
 				System.out.println("Enter the Quantity: ");
 				quantity = scan.nextInt();
-				
 				System.out.println("Enter the Price: ");
 				price = scan.nextFloat();
-				
 				System.out.println("Enter the title: ");
 				title = scan.next();
 				
-				InvMgmt movieObj = new InvMgmt();
-				
-				if (movieObj.addMovie(SKU, quantity, price, title))
-				{	
-					try
-					{
-						FileOutputStream fos = new FileOutputStream("storeInvFile.txt");
-						ObjectOutputStream oos = new ObjectOutputStream(fos);
-						oos.writeObject(movieObj);
-						fos.close();
-					}
-					catch (IOException ex) 
-					{
-						System.out.println(ex.getMessage());
-						System.out.println("Problem with the file output");
-						break;
-			        }
-					
+				if(inv.addMovie(SKU, quantity, price, title))
+				{
 					System.out.println("Success: Movie added");
 				}
+				else
+				{
+					System.out.println("Error: Movie not added");
+				}		
 			}
 			else if(choice == 2)
 			{
-				
+				System.out.println("Enter the SKU: ");
+				SKU = scan.nextInt();
+				if(inv.removeMovie(SKU))
+				{
+					System.out.println("Success: Movie added");
+				}
+				else
+				{
+					System.out.println("Error: Movie not deleted");
+				}
 			}
 			else if(choice == 3)
 			{
-				
-				
+				System.out.println("Enter the SKU: ");
+				SKU = scan.nextInt();
+				inv.displayMovie(SKU);
 			}
 			else if(choice == 4)
 			{
-				
+				inv.displayInventory();
 			}
 			else if(choice == 5)
 			{
+				inv.writeToFile();
 				System.out.println("Exit Selected");
 			}
 			else 
 			{
 				System.out.println("Invalid Option");
 			}
-		}
-		
+		}	
 	}
-
 }
