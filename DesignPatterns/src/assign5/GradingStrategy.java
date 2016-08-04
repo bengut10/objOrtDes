535a0d73a05170c568a36c81a6946befd1ea539a
Ryan Jalufka <ryanjalufka@gmail.com>
2016-08-03 23:30:24 -0500

    Added Javadoc for GradingStrategy.java

--- DesignPatterns/src/assign5/GradingStrategy.java
+++ DesignPatterns/src/assign5/GradingStrategy.java
@@ -2,8 +2,19 @@ package assign5;
 
 import java.util.ArrayList;
 
+/**
+ * GradingStrategy Interface holds the computeAverage method.
+ * @author Jose G.
+ * @author Ryan J.
+ * @version 1.0
+ */
 //Design Pattern: Strategy
 public interface GradingStrategy 
 {
+	/**
+	 * @param exams an ArrayList of exam objects.
+	 * @param assigns an ArrayList of assignment objects.
+	 * @return the average of the student's exams and assigns as a double.
+	 */ 
 	public double computeAverage(ArrayList<Double>exams, ArrayList<Double>assigns);
 }
