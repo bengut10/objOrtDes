9d6638f1cb614b5afb26a01bc8f74bb7af712d84
Ryan Jalufka <ryanjalufka@gmail.com>
2016-08-03 19:04:12 -0500

    Javadoc added for AverageA.java

--- DesignPatterns/src/assign5/AverageA.java
+++ DesignPatterns/src/assign5/AverageA.java
@@ -2,8 +2,22 @@ package assign5;
 
 import java.util.ArrayList;
 
+/**
+ * Class AverageA contains the algorithm for computing the average where the 
+ * Assignment average contributes 40%, and the Exam average contributes 60% 
+ * to the final class average.
+ * @author Jose G.
+ * @author Ryan J.
+ * @version 1.0
+ */
 public class AverageA implements GradingStrategy {
 
+        /**
+         * Computes average of exams and assignments.
+         * @param exams an ArrayList of exams.
+         * @param assigns an Array list of assignments.
+         * @returns the average of the exams and assignments as a double.
+         */
 	@Override
 	public double computeAverage(ArrayList<Double> exams, ArrayList<Double> assigns) {
 		double examTotal = 0;
