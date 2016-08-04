02a96ac2076b66bc212db72474558f3a2ad1bb5e
Ryan Jalufka <ryanjalufka@gmail.com>
2016-08-03 23:38:10 -0500

    Added Javadoc for GradeTracker.java

--- DesignPatterns/src/assign5/GradeTracker.java
+++ DesignPatterns/src/assign5/GradeTracker.java
@@ -3,22 +3,43 @@ package assign5;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * GradeTracker is an observer which updates the student's grade
+ * and converts a double value to a letter grade.
+ * @author Jose G.
+ * @author Ryan J.
+ * @version 1.0
+ */
 //design pattern: Observer
 public class GradeTracker implements Observer {
 
-	String letterGrade = null;
+	String letterGrade = null;	// String variable to hold student's letter grade.
 	
+	
+	/**
+	 * constructs a GradeTracker object
+	 * @param student a Student Object
+	 */
 	public GradeTracker(Student student) 
 	{
 		student.addObserver(this);
 		setLetterGrade(student.getAverage());
 	}
 
+	/**
+	 * Accessor method to get the letter grade.
+	 * @return letterGrade as a String.
+	 */ 
 	public String getLetterGrade() 
 	{
 		return letterGrade;
 	}
 	
+	/**
+	 * Replaces the old student grade with an updated grade.
+	 * @param o an Observable object.
+	 * @param arg an Object 
+	 */ 
 	@Override
 	public void update(Observable o, Object arg) 
 	{
@@ -26,6 +47,10 @@ public class GradeTracker implements Observer {
 		setLetterGrade(student.getAverage());
 	}
 	
+	/**
+	 *Changes double value grade to corresponding letter grade.
+	 * @param grade a double value representing the student's average.
+	 */ 
 	public void setLetterGrade(double grade)
 	{
 		if(grade>= 90)
