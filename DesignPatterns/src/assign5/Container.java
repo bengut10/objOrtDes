04c07e2783dbb733c78f5968d35de11ecf5f02a4
Ryan Jalufka <ryanjalufka@gmail.com>
2016-08-03 22:07:01 -0500

    Added Javadoc for Container.java

--- DesignPatterns/src/assign5/Container.java
+++ DesignPatterns/src/assign5/Container.java
@@ -1,5 +1,11 @@
 package assign5;
 
+/**
+ * Interface Containercalls the getIterator function
+ * @author Jose G.
+ * @author Ryan J.
+ * @version 1.0
+ */
 public interface Container 
 {
 	public Iterator getIterator();
