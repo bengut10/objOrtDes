84711d4cd6f5d2926c1a610ec0087113e3571654
Ryan Jalufka <ryanjalufka@gmail.com>
2016-08-03 19:10:26 -0500

    Added Javadoc for AverageB

--- DesignPatterns/src/assign5/AverageB.java
+++ DesignPatterns/src/assign5/AverageB.java
@@ -1,7 +1,23 @@
 package assign5;
 
 import java.util.ArrayList;
+
+/**
+ * Class AverageB uses AverageA algorithm to compute average,
+ * but drops the lowest Assignment score if there are at least
+ * 2 assignments.
+ * @author Jose G.
+ * @author Ryan J.
+ * @version 1.0
+ */
 public class AverageB implements GradingStrategy {
+	
+	/**
+	 * Computes average of exams and assignments.
+	 * @param exams an ArrayList of exams.
+	 * @param assigns an ArrayList of assignments.
+	 * @returns the average of exams and assignments as a double.
+	 */
 	@Override
 	public double computeAverage(ArrayList<Double> exams, ArrayList<Double> assigns) {
 		
